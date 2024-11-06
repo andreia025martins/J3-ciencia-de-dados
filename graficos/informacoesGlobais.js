68de20b4bea9395618a1c0779f8453446311fb4a
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-06 19:10:38 -0300

    Ajuste tempo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,12 +3,18 @@ const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-Criando
 async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
         const dados = await res.json()
-        console.log(dados)
+        //console.log(dados)
+
+        const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+        const pessoasQuePraticamEsportesRegularmente = (dados.total_pessoas_que_praticam_esportes_regularmente / 1e9)
+        const horas = parseInt(dados.tempo_medio_semana_praticando_esportes)
+        const totalPessoasQueAssistemEsportes = (dados.total_pessoas_que_assistem_esportes /1e9)
+
     
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
-        paragrafo.innerHTML = `Já parou para pensar que somos no mundo mais de <span>${dados.total_pessoas_mundo}</span> Com tantos esportes e novas maneiras de se movimentar, o tempo médio semanal praticando esportes é de <span>${dados.total_pessoas_que_praticam_esportes_regularmente}</span> Embora o número total de pessoas que praticam regularmente seja de cerca de<span>${dados.tempo_medio_semana_praticando_esportes}</span>. eles perdem para os praticantes
-        da "apreciação do esporte", os quais somam quase<span>${dados.total_pessoas_que_assistem_esportes}</span>, que acreditam que podem queimar calorias apenas torcendo
+        paragrafo.innerHTML = `Já parou para pensar que somos no mundo mais de <span>${pessoasMundo}</span> Com tantos esportes e novas maneiras de se movimentar, o tempo médio semanal praticando esportes é de <span>${pessoasQuePraticamEsportesRegularmente}</span> Embora o número total de pessoas que praticam regularmente seja de cerca de <span>${horas}</span>. eles perdem para os praticantes
+        da "apreciação do esporte", os quais somam quase <span>${totalPessoasQueAssistemEsportes}</span>, que acreditam que podem queimar calorias apenas torcendo
         pelo time, como se vibrar na frente da TV contasse como treino, afinal, quem precisa de academia quando se tem o sofá
         como melhor amigo? `
         const container = document.getElementById('graficos-container')
