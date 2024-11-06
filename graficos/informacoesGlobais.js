61ba54ff97a3a6a58fabef1bb2a729502be95d44
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-06 19:28:46 -0300

    alterações no texto

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -13,9 +13,9 @@ async function visualizarInformacoesGlobais() {
     
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
-        paragrafo.innerHTML = `Já parou para pensar que somos no mundo mais de <span>${pessoasMundo}</span> Com tantos esportes e novas maneiras de se movimentar, o tempo médio semanal praticando esportes é de <span>${pessoasQuePraticamEsportesRegularmente}</span> Embora o número total de pessoas que praticam regularmente seja de cerca de <span>${horas}</span>. eles perdem para os praticantes
+        paragrafo.innerHTML = `Já parou para pensar que somos mais de <span>${pessoasMundo}</span> bilhões de pessoas no mundo? <br> Com tantos esportes e novas maneiras de se movimentar, o tempo médio semanal praticando esportes é de <span>${horas}</span> Embora o número total de pessoas que praticam regularmente seja de cerca de <span>${pessoasQuePraticamEsportesRegularmente}</span> bilhões de pessoas. <br>Eles só perdem para os praticantes
         da "apreciação do esporte", os quais somam quase <span>${totalPessoasQueAssistemEsportes}</span>, que acreditam que podem queimar calorias apenas torcendo
-        pelo time, como se vibrar na frente da TV contasse como treino, afinal, quem precisa de academia quando se tem o sofá
+        pelo time. <br>Como se vibrar na frente da TV contasse como treino, afinal, quem precisa de academia quando se tem o sofá
         como melhor amigo? `
         const container = document.getElementById('graficos-container')
         container.appendChild(paragrafo);
